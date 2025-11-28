d3c7f3767b365bef20861616e194f48c7af90475
Gaspar <gasparronaldo37@gmail.com>
2025-11-28 11:43:04 +0200

    feat(app): adicionar CsvApplication com execução automática

--- src/main/java/com/effectivesistemas/csv/CsvApplication.java
+++ src/main/java/com/effectivesistemas/csv/CsvApplication.java
@@ -1,13 +1,26 @@
 package com.effectivesistemas.csv;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.effectivesistemas.csv.presenter.CsvPresenter;
+
 @SpringBootApplication
-public class CsvApplication {
+public class CsvApplication implements CommandLineRunner {
+
+	private final CsvPresenter csvPresenter;
+
+	public CsvApplication(CsvPresenter csvPresenter) {
+		this.csvPresenter = csvPresenter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsvApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		csvPresenter.executarProcessamento();
+	}
 }
